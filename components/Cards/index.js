251b216a5f2f23ff90789705e62e1fa5a4461ccb
bret-knight87 <bret.knight87@gmail.com>
2019-11-10 21:35:19 -0600

    cards complete

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,63 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+const cardContainer = document.querySelector('.cards-container');
+
+axios
+    .get('https://lambda-times-backend.herokuapp.com/articles')
+    .then(response => {
+        console.log(response);
+        response.data.articles.bootstrap.forEach(article => {
+            const finishedArticle = cardCreator(article);
+            cardContainer.appendChild(finishedArticle);
+        })
+        response.data.articles.javascript.forEach(article => {
+            const finishedArticle = cardCreator(article);
+            cardContainer.appendChild(finishedArticle);
+        })
+        response.data.articles.jquery.forEach(article => {
+            const finishedArticle = cardCreator(article);
+            cardContainer.appendChild(finishedArticle);
+        })
+        response.data.articles.node.forEach(article => {
+            const finishedArticle = cardCreator(article);
+            cardContainer.appendChild(finishedArticle);
+        })
+        response.data.articles.technology.forEach(article => {
+            const finishedArticle = cardCreator(article);
+            cardContainer.appendChild(finishedArticle);
+        })
+        
+    })
+
+
+function cardCreator(article) {
+    const newCard = document.createElement('div');
+    newCard.classList.add('card')
+
+    const cardHeadline = document.createElement('div');
+    cardHeadline.classList.add('headline');
+    cardHeadline.textContent = article.headline;
+
+    const authorContainer = document.createElement('div');
+    authorContainer.classList.add('author');
+
+    const cardImg = document.createElement('div');
+    cardImg.classList.add('img-container');
+
+    const imgSrc = document.createElement('img');
+    imgSrc.src = article.authorPhoto;
+
+    const cardAuthor = document.createElement('span');
+    cardAuthor.textContent = `By ${article.authorName}`;
+
+    newCard.appendChild(cardHeadline);
+    newCard.appendChild(authorContainer);
+    authorContainer.appendChild(cardImg);
+    cardImg.appendChild(imgSrc);
+    authorContainer.appendChild(cardAuthor);
+    
+
+    return newCard;
+};
